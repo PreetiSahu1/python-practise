8806a123a98fb78f85ffe80ce64583b697617da0
PreetiSahu1 <preetibhullusahu@gmail.com>
2026-05-19 12:07:21 +0530

    problem 1

---
+++ beginner/fibonacci.py
@@ -0,0 +1,17 @@
+#----begineer----#using while loop 
+n= 10
+a= 0
+b= 1
+z = 0
+while (z <= n):
+    print(z)
+    a=b
+    b=z
+    z= a+b
+
+
+
+#----2----# using for loop
+
+
+
--- beginner/palindrome.py
+++ beginner/palindrome.py
@@ -1,1 +1,20 @@
-print('hello world')
+#----1----#using join reverse
+
+# s = input("Enter the name : ")
+# j= ''.join(reversed(s))
+
+# if s==j:
+#     print("this is palindrome")
+# else:
+#     print("this is not a palindrome")
+
+
+
+
+#----2----# using slicing
+
+s = "nitin"
+if s==s[::1]:
+    print("Palindrome")
+else:
+    print("Not a palindrome")
